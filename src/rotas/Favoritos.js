68483d01355f35f39bbfcc9ec337754bbe79851a
silas-mariosa <silas.mariosa@hotmail.com>
2023-06-15 22:08:47 -0300

    #update

--- src/rotas/Favoritos.js
+++ src/rotas/Favoritos.js
@@ -1,13 +1,77 @@
-import "./App.css";
-import Pesquisa from "componentes/Pesquisa";
-import CardRecomenda from "componentes/CardRecomenda";
-import img from "../imagens/livro2.png"
+import { useEffect } from "react";
+import { useState } from "react";
+import styled from "styled-components";
+import livroImg from "../imagens/livro.png";
+import { getFavoritos } from "servicos/favoritos";
+
+const AppContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(90deg, #002f52 35%, #326589 165%);
+`;
+
+const ResultadoContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+const Resultado = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 20px 0;
+  cursor: pointer;
+  text-align: center;
+  padding: 0 100px;
+  p {
+    width: 200px;
+    color: #fff;
+  }
+  img {
+    width: 100px;
+  }
+  &:hover {
+    border: 1px solid white;
+  }
+`;
+
+const Titulo = styled.h2`
+  color: #fff;
+  font-size: 36px;
+  text-align: center;
+  width: 100%;
+  padding-top: 35px;
+`;
 
 function Favoritos() {
+  const [favoritos, setFavoritos] = useState([]);
+
+  async function fetchFavoritos() {
+    const favoritosFromAPI = await getFavoritos();
+    setFavoritos(favoritosFromAPI);
+  }
+
+  useEffect(() => {
+    fetchFavoritos()
+  }, []);
+
   return (
-    <div className="App">      
-      <Pesquisa />
-    </div>
+    <AppContainer>
+      <div>
+        <Titulo>Aqui estão seus livros favoritos:</Titulo>
+        <ResultadoContainer>
+          {favoritos.length !== 0
+            ? favoritos.map((favorito) => (
+                <Resultado>
+                  <p>{favorito.nome}</p>
+                  <img src={livroImg} />
+                </Resultado>
+              ))
+            : null}
+        </ResultadoContainer>
+      </div>
+    </AppContainer>
   );
 }
 
